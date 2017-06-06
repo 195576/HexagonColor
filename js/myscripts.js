fb22b5c4c00239d5e27f007a3da7095a3006fe17
195576 <195576@edu.p.lodz.pl>
2017-06-06 02:14:58 +0200

    Zmiana widoku na zgodny z wymaganiami. Dodany automatyczny wybór typu palety oraz liczby kolorow na podstawie danych z pliku .geojson.

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,8 @@
-$(function() {
+$(function () {
 
     $('#paletteFrame').hide();
-
+    $('#pickColorFrame').hide();
+    $('#afterSelection').hide();
     // $('#inputSubmitBtn').click(function (evt){
     //
     //     evt.preventDefault();
@@ -9,10 +10,18 @@ $(function() {
     // });
 });
 
+function showSchemas() {
+    $('#pickColorFrame').slideDown(1000);
+}
+
+function hideSchemas() {
+    $('#pickColorFrame').slideUp(500);
+}
+
 var resultPaletteDescription = [];
 
-function submitBtnOnClick () {
-    switch ($('input[name=inlineRadioOptions]:checked', '#radioBtnsForm').val()){
+function submitBtnOnClick() {
+    switch ($('input[name=inlineRadioOptions]:checked', '#radioBtnsForm').val()) {
         case 'option1':
             createSequentialPalette();
             break;
@@ -27,13 +36,14 @@ function submitBtnOnClick () {
 
 var selectedSchema;
 
-function schemaSelection (index) {
+function schemaSelection(index) {
     $("#schema" + selectedSchema).css("background-color", "");
     selectedSchema = index;
     $('#schema' + index).css("background-color", "#9c9c9c");
 }
 
 var points = [];
+var featuresCounter;
 
 $('#InputFile').ready(function () {
     d3.json("geo.json", function (error, data) {
@@ -67,7 +77,7 @@ $('#InputFile').ready(function () {
         //         }
         //     }
         // }
-
+        featuresCounter = data.features.length;
         document.getElementById('numberOfFeatures').innerHTML = 'There are ' + data.features.length + ' features in this file.';
 
         loadDataFromFile(data);
@@ -93,10 +103,11 @@ $('#InputFile').ready(function () {
 });
 
 function createSequentialPalette() {
+    $('#palette').html("");
     var numberOfColours = $('#nOdataClasses').val();
-    var palette = "<div class=\"paletteSelection\"><svg width=\"15\" height=\"" + numberOfColours * 15 + "\">";
+    var palette = "<div><svg height=\"25\" width=\"" + numberOfColours * 25 + "\">";
     var startColor = "white";
-    var stopColor="red";
+    var stopColor = "red";
 
     switch (selectedSchema) {
         case 1:
@@ -115,7 +126,7 @@ function createSequentialPalette() {
             startColor = 'rgb(255,255,178)';
             stopColor = "rgb(179,0,0)";
             break;
-    };
+    }
 
     var color = d3.scaleLinear()
         .domain([0, 10 * numberOfColours])
@@ -124,21 +135,22 @@ function createSequentialPalette() {
     var i;
     for (i = 0; i < numberOfColours; i++) {
         resultPaletteDescription[i] = color(10 * i);
-        palette += '<rect fill="' + resultPaletteDescription[i] + '" width="15" height="15" y="' + i * 15 + '"></rect>';
+        palette += '<rect fill="' + resultPaletteDescription[i] + '" width="25" height="25" x="' + i * 25 + '"></rect>';
     }
     palette += "</svg></div>";
-    var divElement  = document.createElement('div');
+    var divElement = document.createElement('div');
     divElement.innerHTML = palette;
     $('#palette')[0].appendChild(divElement);
 
-    for (i = 0; i < numberOfColours; ++i) {
-        document.getElementById("description").innerHTML += resultPaletteDescription[i] + ', ' + '<br>';
-    }
+    // for (i = 0; i < numberOfColours; ++i) {
+    //     document.getElementById("description").innerHTML += resultPaletteDescription[i] + ', ' + '<br>';
+    // }
 
     $('#paletteFrame').slideDown(1000);
 }
 
 function createQualitativePalette() {
+    $('#palette').html("");
     var numberOfColours = $('#nOdataClasses').val();
     var palette = "<div class=\"paletteSelection\"><svg width=\"15\" height=\"" + numberOfColours * 15 + "\">";
 
@@ -147,10 +159,10 @@ function createQualitativePalette() {
 
     var i;
     for (i = 0; i < numberOfColours; i++) {
-        palette += '<rect fill="' + color(10*i) + '" width="15" height="15" y="' + i * 15 + '"></rect>';
+        palette += '<rect fill="' + color(10 * i) + '" width="15" height="15" y="' + i * 15 + '"></rect>';
     }
     palette += "</svg></div>";
-    var divElement  = document.createElement('div');
+    var divElement = document.createElement('div');
     divElement.innerHTML = palette;
     $('#palette')[0].appendChild(divElement);
 
@@ -158,6 +170,7 @@ function createQualitativePalette() {
 }
 
 function createDivergingPalette() {
+    $('#palette').html("");
     var numberOfColours = $('#nOdataClasses').val();
     var palette = "<div class=\"paletteSelection\"><svg width=\"15\" height=\"" + numberOfColours * 15 + "\">";
 
@@ -166,20 +179,23 @@ function createDivergingPalette() {
 
     var i;
     for (i = 0; i < numberOfColours; i++) {
-        palette += '<rect fill="' + color(10*i) + '" width="15" height="15" y="' + i * 15 + '"></rect>';
+        palette += '<rect fill="' + color(10 * i) + '" width="15" height="15" y="' + i * 15 + '"></rect>';
     }
     palette += "</svg></div>";
-    var divElement  = document.createElement('div');
+    var divElement = document.createElement('div');
     divElement.innerHTML = palette;
     $('#palette')[0].appendChild(divElement);
 
     $('#paletteFrame').slideDown(1000);
+}
 
-function loadDataFromFile (data) {
+function loadDataFromFile(data) {
 
     var propertyKeys = Object.keys(data.features[0].properties);
-    var ret = propertyKeys.map(function(key){
-        return data.features.map(function(feature){ return feature.properties[key]});
+    var ret = propertyKeys.map(function (key) {
+        return data.features.map(function (feature) {
+            return feature.properties[key]
+        });
     });
     var tb = document.querySelector("#summary");
     var tbody = tb.querySelector("tbody");
@@ -187,22 +203,27 @@ function loadDataFromFile (data) {
     ret.forEach(function (v, i) {
         var row = document.createElement("tr");
         var uniq = _.uniq(v).length === v.length;
-        var type = _.uniq(v.map(function(x){return typeof x}));
+        var type = _.uniq(v.map(function (x) {
+            return typeof x
+        }));
         var first3 = _.uniq(v).slice(0, 3);
         var min, max, avg, tot, count;
-        if (type.length == 1 && type[0] == "number") {
+        if (type.length === 1 && type[0] === "number") {
             min = _.min(v);
             max = _.max(v);
             tot = _.sum(v).toFixed(4);
             count = v.length;
-            avg = (tot/count).toFixed(4);
+            avg = (tot / count).toFixed(4);
         }
-        var results = [propertyKeys[i], first3.join(", "), uniq, type.join(", "), min, avg, max];
-        var classes = ["property", "first3", "unique", "types", "min", "avg", "max"];
-        results.forEach(function(r, j){
+        row.onclick = function () {
+            onTableRowSelect(row);
+        };
+        var results = [propertyKeys[i], first3.join(", "), uniq, type.join(", "), min, avg, max, count];
+        var classes = ["property", "first3", "unique", "types", "min", "avg", "max", "count"];
+        results.forEach(function (r, j) {
             var cell = document.createElement("td");
             cell.classList.add(classes[j]);
-            cell.innerHTML = r === undefined ? "-":r.toString();
+            cell.innerHTML = r === undefined ? "-" : r.toString();
             row.appendChild(cell);
         });
         if (uniq && type.length === 1) {
@@ -213,5 +234,37 @@ function loadDataFromFile (data) {
         }
         tbody.appendChild(row);
     });
-
 }
+
+var onTableRowSelect = function (row) {
+    $('#inlineRadio1').attr('checked', false);
+    $('#inlineRadio2').attr('checked', false);
+    $('#inlineRadio3').attr('checked', false);
+    if (row.childNodes[3].innerHTML !== 'number') {
+        if (row.childNodes[3].innerHTML === 'string') {
+            $('#inlineRadio3').click();
+        }
+    } else {
+        if (row.childNodes[2].innerHTML === 'true') {
+            $('#inlineRadio2').click();
+        } else {
+            $('#inlineRadio1').click();
+        }
+    }
+    if (featuresCounter <= 10) {
+        $('#nOdataClasses').val(featuresCounter);
+    }
+    else {
+        var i = 10;
+        for (i; i >= 2; --i) {
+            if (featuresCounter % i === 0) {
+                $('#nOdataClasses').val(i);
+                break;
+            }
+        }
+        if (i < 2) {
+            // console.log("pierwsza");
+        }
+    }
+    $('#afterSelection').slideDown(500);
+};
